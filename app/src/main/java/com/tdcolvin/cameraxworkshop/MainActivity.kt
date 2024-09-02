0bbdbc1f944eb6068b4df5416e6b8d0759eb4c6f
Tom Colvin <tom.colvin@apptaura.com>
2024-09-02 15:26:48 +0100

    added control buttons layout, so we don't have to do this manually in the workshop

--- app/src/main/java/com/tdcolvin/cameraxworkshop/MainActivity.kt
+++ app/src/main/java/com/tdcolvin/cameraxworkshop/MainActivity.kt
@@ -8,10 +8,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.CameraSelector
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.FileProvider
@@ -40,7 +47,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CameraAppScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        //add the preview view here
 
+        ControlButtons(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            onZoom = { zoomLevel -> },
+            onSelectCamera = { cameraFacing ->  },
+            onCapture = { }
+        )
+    }
+}
+
+@Composable
+fun ControlButtons(
+    modifier: Modifier = Modifier,
+    onZoom: (zoomLevel: Float) -> Unit,
+    onSelectCamera: (cameraFacing: Int) -> Unit,
+    onCapture: () -> Unit
+) {
+    Column(modifier = modifier) {
+        Row {
+            Button(onClick = { onSelectCamera(CameraSelector.LENS_FACING_FRONT) }) {
+                Text("Front camera")
+            }
+            Button(onClick = { onSelectCamera(CameraSelector.LENS_FACING_BACK) }) {
+                Text("Back camera")
+            }
+        }
+
+        Row {
+            Button(onClick = { onZoom(0.0f) }) {
+                Text("Zoom 0.0")
+            }
+            Button(onClick = { onZoom(0.5f) }) {
+                Text("Zoom 0.5")
+            }
+            Button(onClick = { onZoom(1.0f) }) {
+                Text("Zoom 1.0")
+            }
+        }
+
+        Button(onClick = onCapture) {
+            Text("Take Photo")
+        }
+    }
 }
 
 @Preview(showBackground = true)
